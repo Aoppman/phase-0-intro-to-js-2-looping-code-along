437e00ea64e35b5022c4fe8d5192b3dc33af8ab1
Andrew <a.oppman@gmail.com>
2023-03-13 05:55:16 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const name = ["Guadalupe", "Ollie", "Aki"];
+const messages = []
+
+function writeCards(names, eventNames) {
+    for (let a = 0; a < names.length; a ++) {
+        let message = `Thank you, ${names[a]}, for the wonderful ${eventNames} gift!`;
+        messages.push(message);
+    }
+return messages
+}
+
+function countDown(number) {
+    while (number >= 0)
+    { console.log(number);
+    number --;}
+}
